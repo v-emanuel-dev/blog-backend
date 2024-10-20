0ee463b01df9733f0ac78bd506cb3f8e47a51672
Victor Emanuel <123639739+v-emanuel-dev@users.noreply.github.com>
2024-10-20 04:36:57 -0300

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,33 +1,22 @@
 const jwt = require('jsonwebtoken');
-const db = require('../config/db'); // Ajuste conforme necessário
 
 exports.verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
 
   if (!token) {
-    console.error("No token provided");
-    return res.status(401).json({ error: 'No token provided' });
+    console.log("No token provided, proceeding as guest");
+    req.userId = null; // Ou qualquer valor que indique que é um usuário não autenticado
+    return next();
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       console.error("Failed to authenticate token:", err);
-      return res.status(403).json({ error: 'Failed to authenticate token' });
+      req.userId = null; // Se o token falhar, tratá-lo como um visitante
+      return next();
     }
 
     req.userId = decoded.id;
-    console.log("Decoded userId from token:", req.userId);
-
-    // Buscar o papel do usuário no banco de dados
-    db.query('SELECT role FROM users WHERE id = ?', [req.userId], (error, results) => {
-      if (error || results.length === 0) {
-        console.error("Failed to retrieve user role:", error);
-        return res.status(500).json({ error: 'Failed to retrieve user role' });
-      }
-
-      req.userRole = results[0].role;
-      console.log("User role retrieved:", req.userRole);
-      next();
-    });
+    next();
   });
 };

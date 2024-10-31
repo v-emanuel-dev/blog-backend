5f020fe0abcf614070cad01116d82dc5a5aa0288
Victor Emanuel <v.emanuel.pacheco@gmail.com>
2024-10-31 05:03:41 -0300

    beta release version 2.0

--- config/passport.js
+++ config/passport.js
@@ -5,32 +5,28 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://blog-backend-production-c203.up.railway.app/api/auth/google/callback",
+  callbackURL: "http://localhost:3000/api/auth/google/callback",
 }, (accessToken, refreshToken, profile, done) => {
   
   const email = profile.emails[0].value;
-  const username = profile.displayName.replace(/\s+/g, '').toLowerCase(); // Ajuste do nome de usuário
+  const username = profile.displayName.replace(/\s+/g, '').toLowerCase();
   const profilePicture = profile.photos[0]?.value || null;
 
   User.findByEmail(email, (err, user) => {
     if (err) {
-      console.error('Error finding user by email:', err);
       return done(err);
     }
 
     if (user) {
       return done(null, user);
     } else {
-      console.log('User not found, creating a new one');
-      // Usuário não encontrado, criar um novo
       User.create({
         email,
         username,
-        password: 'dummyhashedpassword', // Placeholder, já que a senha não é relevante para login com Google
+        password: 'dummyhashedpassword',
         profilePicture,
       }, (err, newUser) => {
         if (err) {
-          console.error('Error creating new user:', err);
           return done(err);
         }
         return done(null, newUser);
@@ -40,15 +36,12 @@ passport.use(new GoogleStrategy({
 }));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id); // Serializa apenas o ID do usuário
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => {
-    if (err) {
-      console.error('Error deserializing user:', err);
-    }
-    done(err, user); // Recupera o usuário do banco de dados
+    done(err, user);
   });
 });
 
